8b2ecefc56ccc8dd82325f913ba8c28ff3b0767d
Glitch (superficial-monday-fujrnj7456) <none>
2020-01-18 07:49:11 +0000

    Import of Androz2091/Discbot.js

---
+++ config.sample.js
@@ -0,0 +1,46 @@
+module.exports = {
+
+    /* YOUR DISCORD BOT TOKEN */
+    token: "Your Discord Bot Token here",
+
+    /* DEFAULT GUILD CONFIGURATION */
+    guildConf: {
+        language: "english",
+        prefix: "disc!"
+    },
+    
+    /* THE MONGODB URL */
+    mongoDB: "mongodb://localhost:27017/Discbot",
+
+    /* EMOJIS STRINGS LIKE <:emojiName:emojiID> */
+    emojis: {
+        success: "XXXXXXXXX",
+        error: "XXXXXXXXX",
+        loading: "XXXXXXXXX"
+    },
+
+    /* ARRAY OF BOT'S OWNERS IDs */
+    owners: [],
+
+    /* EMBED CONFIGURATION */
+    embed: {
+        color: "#FF0000",
+        footer: "Discbot | Open source Discord bot template"
+    },
+
+    /* STATUS LIST (Playing to... or listen to...) */
+    status: {
+        updateEvery: 20000,
+        list: [
+            {
+                type: "PLAYING",
+                content: "On {guildsCount} servers!"
+            },
+            {
+                type: "LISTENING",
+                content: "{usersCount} users"
+            }
+        ]
+    }
+
+};
