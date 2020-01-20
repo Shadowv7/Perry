8f0980a5958b9d0e588c374785889f76ef666dd6
Glitch (superficial-monday-fujrnj7456) <none>
2020-01-20 15:10:55 +0000

    🍴🐙 Checkpoint ./commands/Moderations/setlang.js:7631065/6334

---
+++ commands/Moderations/setlang.js
@@ -0,0 +1,39 @@
+const Command = require("../../structures/Command.js"),
+  Discord = require("discord.js");
+
+class Lang extends Command {
+  constructor(client) {
+    super(client, {
+      name: "setlang",
+      description: language => language.get("LANG_DESCRIPTION"),
+      usage: language => language.get("LANG_USAGE"),
+      examples: language => language.get("LANG_EXEMPLES"),
+      enabled: true,
+      aliases: [],
+      clientPermissions: [],
+      memberPermissions: ["MANAGE_GUILD"],
+      permLevel: 3,
+      cooldown: 5000,
+      commandPath: __dirname,
+      guildOnly: true,
+      nsfw: false
+    });
+  }
+
+  async run(message, args, data) {
+    if (!args[0] || !["fr", "en", "français", "english"].includes(args[0]))
+      return message.reply(message.language.get("LANG_NO_ARGS"));
+    const languages = {
+      fr: "french",
+      français: "french",
+      en: "english",
+      english: "english"
+    };
+
+    if (this.client.settings.get(message.guild.id, "language") === languages[args[0]])
+      return message.reply(message.language.get("LANG_ALREADY", languages[args[0]]));
+    this.client.settings.set(message.guild.id, languages[args[0]], "language");
+    message.reply(message.language.get("LANG_SUCCESS", args[0]));
+  }
+}
+module.exports = Lang;
