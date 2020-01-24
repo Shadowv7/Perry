a69e3a37c69b3b6d99346a0ab49ef7ec1ae8856b
Glitch (superficial-monday-fujrnj7456) <none>
2020-01-24 09:21:06 +0000

    🎰🏊🏼 Checkpoint ./events/guildMemberBoost.js:7631065/3151

--- events/guildMemberBoost.js
+++ events/guildMemberBoost.js
@@ -4,16 +4,20 @@ module.exports = class {
   }
 
   async run(member) {
-    const channel = member.channel.find(
-      c => c.id === this.client.settings.get(member.guild.id, "logschannel")
+    const channel = member.guild.channels.get(
+      this.client.settings.get(member.guild.id, "logs-channel")
     );
     if (!channel) return;
     channel.send({
       embed: {
-        color: 0x2BFAFA,
+        color: 0x2bfafa,
         title: member.language.get("BOOSTER_TITLE"),
-        description: member.language.get("BOOSTER_MESSAGE",member,member.guild.name)
+        description: member.language.get(
+          "BOOSTER_MESSAGE",
+          member
+        )
       }
     });
   }
 };
+  
