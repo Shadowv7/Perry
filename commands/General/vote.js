5ff5d7d674900c4522ffba37094ca8945567de5a
Glitch (superficial-monday-fujrnj7456) <none>
2020-02-02 10:34:51 +0000

    🌷🌦 Checkpoint ./.gitignore:7631065/30 ./config.js:7631065/240 ./commands/General/vote.js:7631065/320

--- commands/General/vote.js
+++ commands/General/vote.js
@@ -27,11 +27,7 @@ class Vote extends Command {
       .setColor("2BFAFA")
       .setTitle("•__Vote__•")
       .setDescription(
-        "<:arcane:568162416274440202> [Arcane Bot Center](https://arcane-botcenter.xyz/bot/658579503135588392)" +
-          "\n" +
-          "<:logo:459634405183586304> [Divine Discord Bot List](https://divinediscordbots.com/bot/658579503135588392/vote)" +
-          "\n" +
-          "<:discordbotlist:338808864352763904> [Top.gg](https://top.gg/bot/658579503135588392/vote)"
+        `<:arcane:568162416274440202> [Arcane Bot Center](https://arcane-botcenter.xyz/bot/${this.client.id})` 
       )
       .setTimestamp()
       .setFooter(
